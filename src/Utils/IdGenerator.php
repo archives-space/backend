ab33843ad8a25cac040a156e7bdff22fdec9444a
Matthieu Bessat <spamfree@matthieubessat.fr>
2021-04-25 11:50:47 +0200

    feat(IdGenerator): add base64 support

--- src/Utils/IdGenerator.php
+++ src/Utils/IdGenerator.php
@@ -7,24 +7,25 @@ use Doctrine\ORM\Id\AbstractIdGenerator;
 
 class IdGenerator extends AbstractIdGenerator
 {
+    private static string $alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-';
+
     public static function generateHex(int $len): string
     {
         $base = hash('sha256', random_bytes(8));
         return strtoupper(substr($base, 0, $len));
     }
 
-    public static function generateStr(int $len): string
+    public static function generateStr(int $len = 64): string
     {
-        $letters = 'abcdefghijklmnopqrstABCDEFGHIJKLMNPQRSTUVWXYZ123456789';
         $out = '';
         for ($i = 0; $i < $len; $i++) {
-            $out .= $letters[random_int(0, strlen($letters) - 1)];
+            $out .= self::$alphabet[random_int(0, strlen(self::$alphabet) - 1)];
         }
         return $out;
     }
 
     public function generate(EntityManager $em, $entity): string
     {
-        return (string)self::generateStr(12);
+        return (string)self::generateStr();
     }
 }
